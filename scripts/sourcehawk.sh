814df01a1d3ab13732919d79c62d4f17466ab4dc
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-09-13 09:52:59 -0300

    test ci

--- scripts/sourcehawk.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-echo "\n#### Running validation"
-
-docker run -v "$(pwd):/home/sourcehawk" optumopensource/sourcehawk:0.6.0 scan
-
-echo "#### Validation pass"
-exit 0
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-echo "\n#### Running tests"
-
-diff -y --suppress-common-lines build/ tests/expected/
-
-echo "#### Tests pass"
-exit 0
